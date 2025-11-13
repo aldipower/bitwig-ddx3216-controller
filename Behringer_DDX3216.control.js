e848a5f13731bd6b7237acf910bc44413fcfa2bc
Felix Gertz <dev@felixgertz.de>
2025-11-13 16:09:28 +0100

    Set Bitwig volume fader by DDX sysex value

--- Behringer_DDX3216.control.js
+++ Behringer_DDX3216.control.js
@@ -3,7 +3,7 @@ const EXTENSION_NAME = "DDX3216";
 const VERSION = "0.7.0";
 const AUTHOR = "Felix Gertz";
 const MIDI_CHANNEL = 0; // 0 == channel 1.
-const DEVICE_ID = 0x7F;
+const DEVICE_ID = 0x7f;
 const NUM_FADERS = 32;
 const FADER_CC_BASE = 1; // CC 1..32 -> faders 1..32
 const MUTE_CC_BASE = 33; // CC 33..64 -> mute buttons 1..32
@@ -14,9 +14,8 @@ function getDeviceByte(deviceId) {
     if (deviceId === undefined) {
         return 0x60;
     }
-    return 0x40 | (deviceId & 0x0F);
+    return 0x40 | (deviceId & 0x0f);
 }
-;
 function constructSysEx(command) {
     return `F0002032${getDeviceByte(DEVICE_ID)}0B${command}F7`;
 }
@@ -32,10 +31,12 @@ let cursorTrack;
 // Takes and sends the sysex volume value from 0 to 1472
 // ddx_dB = -80 + value/16
 function sendSysExVolumeToMixer(faderIndex, sysExVolume) {
-    const high7bit = ((sysExVolume >> 7) & 0x7F).toString(16).padStart(2, '0');
-    const low7bit = (sysExVolume & 0x7F).toString(16).padStart(2, '0');
+    const high7bit = ((sysExVolume >> 7) & 0x7f).toString(16).padStart(2, "0");
+    const low7bit = (sysExVolume & 0x7f).toString(16).padStart(2, "0");
     // PARAMCHANGE_FUNC COUNT CHANNEL/FADERINDEX FUNCTION_CODE VALUE
-    const command = `2001${faderIndex.toString(16).padStart(2, '0')}01${high7bit}${low7bit}`.toUpperCase();
+    const command = `2001${faderIndex
+        .toString(16)
+        .padStart(2, "0")}01${high7bit}${low7bit}`.toUpperCase();
     const sysex = constructSysEx(command);
     // println(`sysex cmd ${sysex}`)
     midiOut.sendSysex(sysex);
@@ -53,13 +54,33 @@ function displayedVolumeChanged(faderIndex, bitwigDisplayValue) {
         dbVolume = 12;
     }
     const sysExVolume = Math.round((dbVolume + 80) * 16);
-    println(`Volume display: ${dbVolume} ${bitwigDisplayValue.includes("Inf")} sysex ${sysExVolume}`);
+    // println(`Volume display: ${dbVolume} ${bitwigDisplayValue.includes("Inf")} sysex ${sysExVolume}`);
     sendSysExVolumeToMixer(faderIndex, sysExVolume);
     // midiOut.sendMidi(0xB0 | MIDI_CHANNEL, FADER_CC_BASE + faderIndex, ccVal);
     // host.scheduleTask(() => {
     //   const displayedValue = track.volume().displayedValue().get();
     //   println(`Volume value ${value} display: ${displayedValue}`);
     // }, 0);
+}
+function dbToNormalized(db) {
+    if (db <= -80.0) {
+        return 0;
+    }
+    if (db >= 6.0) {
+        return 1;
+    }
+    return Math.pow(10, (db - 6) / 60);
+}
+function setBitwigFaderVolumeBySysexValue(faderIndex, sysexVolume) {
+    try {
+        const track = trackBank.getItemAt(faderIndex);
+        const dbValue = sysexVolume / 16 - 80;
+        const normalizedVolume = dbToNormalized(dbValue);
+        track.volume().setImmediately(normalizedVolume);
+    }
+    catch (error) {
+        errorln(`Could not set Bitwig fader volume by sysex ${error}`);
+    }
 }
 function init() {
     midiIn = host.getMidiInPort(0);
@@ -73,13 +94,60 @@ function init() {
         t.volume().markInterested();
         t.pan().markInterested();
         t.mute().markInterested();
-        t.volume().displayedValue().addValueObserver((displayedValue) => {
-            displayedVolumeChanged(i, displayedValue);
-        });
+        // t.volume().displayedValue().addValueObserver((displayedValue) => {
+        //   displayedVolumeChanged(i, displayedValue);
+        // });
         // t.volume().value().addRawValueObserver((value) => {
         //   // sendVolumeChangeToMixer(i, value);
         // });
     }
+    midiIn.setSysexCallback((sysexData) => {
+        println(`Sysex received ${sysexData}`);
+        if (!sysexData.startsWith("f0002032")) {
+            return;
+        }
+        // PARAMCHANGE_FUNC COUNT  CHANNEL/FADERINDEX FUNCTION_CODE VALUE
+        // const command = `2001${faderIndex
+        //   .toString(16)
+        //   .padStart(2, "0")}01${high7bit}${low7bit}`.toUpperCase();
+        const regexp = /f0002032([0-F]{2})0b([0-F]{2})([0-F]{2})(.*)F7/gim;
+        const [, midiChannel, functionType, msgCount, messagesString] = regexp.exec(sysexData);
+        const messages = messagesString.match(/.{1,8}/g);
+        // F0002032 ${getDeviceByte(DEVICE_ID)}0B ${command}F7
+        // f0002032 00 0b 20 02 0001082e 0101083a f7
+        // Sysex received f0002032000b20020001032101010268f7
+        // Midi Channel: 00 fnType: 20 msgCount: 02 messages: 00010321|01010268
+        // TODO: Check for MIDI channel
+        println(`Midi Channel: ${midiChannel} fnType: ${functionType} msgCount: ${msgCount} messages: ${messages.join("|")}`);
+        if (parseInt(msgCount, 16) !== messages.length) {
+            errorln(`Message count does not match messages length`);
+        }
+        // Parameter change
+        if (functionType === "20") {
+            if (messages == null) {
+                println("messages is null");
+                return;
+            }
+            messages.forEach((message, i) => {
+                if (message == null) {
+                    errorln("message is null");
+                    return;
+                }
+                const paramRegex = /([0-F]{2})([0-F]{2})([0-F]{2})([0-F]{2})/gim;
+                const [, faderIndex, functionCode, highWord, lowWord] = paramRegex.exec(message);
+                let sysexValue = Number((parseInt(highWord, 16) << 7) | parseInt(lowWord, 16));
+                println(`faderIndex: ${faderIndex} fnType: ${functionCode} highWord: ${highWord} lowWord: ${lowWord} sysexValue: ${sysexValue}`);
+                if (isNaN(sysexValue)) {
+                    return;
+                }
+                const faderIndexInt = parseInt(faderIndex, 16);
+                // Volume
+                if (functionCode === "01") {
+                    setBitwigFaderVolumeBySysexValue(faderIndexInt, sysexValue);
+                }
+            });
+        }
+    });
     println(`${VENDOR} ${EXTENSION_NAME} controller script version ${VERSION} written by ${AUTHOR} initialized. This script comes under GPLv3 license.`);
 }
 function exit() {
@@ -87,3 +155,4 @@ function exit() {
         displayedVolumeChanged(i, "-80");
     }
 }
+function flush() { }
--- src/Behringer_DDX3216.control.ts
+++ src/Behringer_DDX3216.control.ts
@@ -4,7 +4,7 @@ const VERSION = "0.7.0";
 const AUTHOR = "Felix Gertz";
 
 const MIDI_CHANNEL = 0; // 0 == channel 1.
-const DEVICE_ID = 0x7F;
+const DEVICE_ID = 0x7f;
 const NUM_FADERS = 32;
 
 const FADER_CC_BASE = 1; // CC 1..32 -> faders 1..32
@@ -17,16 +17,22 @@ const FEEDBACK_INTERVAL_MS = 200;
 function getDeviceByte(deviceId?: number) {
   if (deviceId === undefined) {
     return 0x60;
-  } 
-  return 0x40 | (deviceId & 0x0F);
-};
+  }
+  return 0x40 | (deviceId & 0x0f);
+}
 
 function constructSysEx(command: string) {
   return `F0002032${getDeviceByte(DEVICE_ID)}0B${command}F7`;
 }
 
 loadAPI(24);
-host.defineController(VENDOR, EXTENSION_NAME, VERSION, "57fb8818-a6a6-4a23-9413-2a1a5aea3ce1", AUTHOR);
+host.defineController(
+  VENDOR,
+  EXTENSION_NAME,
+  VERSION,
+  "57fb8818-a6a6-4a23-9413-2a1a5aea3ce1",
+  AUTHOR
+);
 host.setShouldFailOnDeprecatedUse(true);
 host.defineMidiPorts(1, 1);
 host.addDeviceNameBasedDiscoveryPair(["DDX3216"], ["DDX3216"]);
@@ -39,11 +45,13 @@ let cursorTrack: API.CursorTrack;
 // Takes and sends the sysex volume value from 0 to 1472
 // ddx_dB = -80 + value/16
 function sendSysExVolumeToMixer(faderIndex: number, sysExVolume: number) {
-  const high7bit = ((sysExVolume >> 7) & 0x7F).toString(16).padStart(2,'0');
-  const low7bit  = (sysExVolume & 0x7F).toString(16).padStart(2,'0');
+  const high7bit = ((sysExVolume >> 7) & 0x7f).toString(16).padStart(2, "0");
+  const low7bit = (sysExVolume & 0x7f).toString(16).padStart(2, "0");
 
   // PARAMCHANGE_FUNC COUNT CHANNEL/FADERINDEX FUNCTION_CODE VALUE
-  const command = `2001${faderIndex.toString(16).padStart(2,'0')}01${high7bit}${low7bit}`.toUpperCase()
+  const command = `2001${faderIndex
+    .toString(16)
+    .padStart(2, "0")}01${high7bit}${low7bit}`.toUpperCase();
 
   const sysex = constructSysEx(command);
 
@@ -52,7 +60,10 @@ function sendSysExVolumeToMixer(faderIndex: number, sysExVolume: number) {
   midiOut.sendSysex(sysex);
 }
 
-function displayedVolumeChanged(faderIndex: number, bitwigDisplayValue: string) {
+function displayedVolumeChanged(
+  faderIndex: number,
+  bitwigDisplayValue: string
+) {
   let dbVolume = parseFloat(bitwigDisplayValue);
   const isInf = bitwigDisplayValue.includes("Inf");
 
@@ -68,7 +79,7 @@ function displayedVolumeChanged(faderIndex: number, bitwigDisplayValue: string) 
 
   const sysExVolume = Math.round((dbVolume + 80) * 16);
 
-  println(`Volume display: ${dbVolume} ${bitwigDisplayValue.includes("Inf")} sysex ${sysExVolume}`);
+  // println(`Volume display: ${dbVolume} ${bitwigDisplayValue.includes("Inf")} sysex ${sysExVolume}`);
 
   sendSysExVolumeToMixer(faderIndex, sysExVolume);
 
@@ -76,9 +87,36 @@ function displayedVolumeChanged(faderIndex: number, bitwigDisplayValue: string) 
 
   // host.scheduleTask(() => {
   //   const displayedValue = track.volume().displayedValue().get();
-  
+
   //   println(`Volume value ${value} display: ${displayedValue}`);
   // }, 0);
+}
+
+function dbToNormalized(db) {
+  if (db <= -80.0) {
+    return 0;
+  } 
+  if (db >= 6.0) {
+    return 1;
+  } 
+
+  return Math.pow(10, (db - 6) / 60);
+}
+
+function setBitwigFaderVolumeBySysexValue(
+  faderIndex: number,
+  sysexVolume: number
+) {
+  try {
+    const track = trackBank.getItemAt(faderIndex);
+  
+    const dbValue = sysexVolume / 16 - 80;
+    const normalizedVolume = dbToNormalized(dbValue);
+  
+    track.volume().setImmediately(normalizedVolume);
+  } catch (error) {
+    errorln(`Could not set Bitwig fader volume by sysex ${error}`);
+  }
 }
 
 function init() {
@@ -96,16 +134,93 @@ function init() {
     t.pan().markInterested();
     t.mute().markInterested();
 
-    t.volume().displayedValue().addValueObserver((displayedValue) => {
-      displayedVolumeChanged(i, displayedValue);
-    });
+    // t.volume().displayedValue().addValueObserver((displayedValue) => {
+    //   displayedVolumeChanged(i, displayedValue);
+    // });
 
     // t.volume().value().addRawValueObserver((value) => {
     //   // sendVolumeChangeToMixer(i, value);
     // });
   }
-  
-  println(`${VENDOR} ${EXTENSION_NAME} controller script version ${VERSION} written by ${AUTHOR} initialized. This script comes under GPLv3 license.`);
+
+  midiIn.setSysexCallback((sysexData) => {
+    println(`Sysex received ${sysexData}`);
+
+    if (!sysexData.startsWith("f0002032")) {
+      return;
+    }
+
+    // PARAMCHANGE_FUNC COUNT  CHANNEL/FADERINDEX FUNCTION_CODE VALUE
+    // const command = `2001${faderIndex
+    //   .toString(16)
+    //   .padStart(2, "0")}01${high7bit}${low7bit}`.toUpperCase();
+
+    const regexp = /f0002032([0-F]{2})0b([0-F]{2})([0-F]{2})(.*)F7/gim;
+    const [, midiChannel, functionType, msgCount, messagesString] =
+      regexp.exec(sysexData);
+    const messages = messagesString.match(/.{1,8}/g);
+
+    // F0002032 ${getDeviceByte(DEVICE_ID)}0B ${command}F7
+    // f0002032 00 0b 20 02 0001082e 0101083a f7
+
+    // Sysex received f0002032000b20020001032101010268f7
+    // Midi Channel: 00 fnType: 20 msgCount: 02 messages: 00010321|01010268
+
+    // TODO: Check for MIDI channel
+
+    println(
+      `Midi Channel: ${midiChannel} fnType: ${functionType} msgCount: ${msgCount} messages: ${messages.join(
+        "|"
+      )}`
+    );
+
+    if (parseInt(msgCount, 16) !== messages.length) {
+      errorln(`Message count does not match messages length`);
+    }
+
+    // Parameter change
+    if (functionType === "20") {
+      if (messages == null) {
+        println("messages is null");
+        return;
+      }
+
+      messages.forEach((message, i) => {
+        if (message == null) {
+          errorln("message is null")
+          return;
+        }
+
+        const paramRegex = /([0-F]{2})([0-F]{2})([0-F]{2})([0-F]{2})/gim;
+
+        const [, faderIndex, functionCode, highWord, lowWord] =
+          paramRegex.exec(message);
+
+        let sysexValue = Number(
+          (parseInt(highWord, 16) << 7) | parseInt(lowWord, 16)
+        );
+
+        println(
+          `faderIndex: ${faderIndex} fnType: ${functionCode} highWord: ${highWord} lowWord: ${lowWord} sysexValue: ${sysexValue}`
+        );
+
+        if (isNaN(sysexValue)) {
+          return;
+        }
+
+        const faderIndexInt = parseInt(faderIndex, 16);
+
+        // Volume
+        if (functionCode === "01") {
+          setBitwigFaderVolumeBySysexValue(faderIndexInt, sysexValue);
+        }
+      });
+    }
+  });
+
+  println(
+    `${VENDOR} ${EXTENSION_NAME} controller script version ${VERSION} written by ${AUTHOR} initialized. This script comes under GPLv3 license.`
+  );
 }
 
 function exit() {
@@ -113,3 +228,5 @@ function exit() {
     displayedVolumeChanged(i, "-80");
   }
 }
+
+function flush() {}
